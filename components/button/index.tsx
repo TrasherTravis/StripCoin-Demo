ea84cb466f9214a2cd3389e576b12d7ed266749f
TrasherTravis <trashertravis@gmail.com>
2021-08-12 19:13:44 +0530

    Deleted

--- components/button/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {ButtonStyled} from './styles';
-
-const Button: React.FC<{ name: string; onClick: () => void, isInactive?: boolean; }> = ({name, onClick, isInactive}) => {
-
-   return <ButtonStyled onClick={isInactive ? null : onClick} isInactive={isInactive}>{name}</ButtonStyled>
-}
-
-export default Button
