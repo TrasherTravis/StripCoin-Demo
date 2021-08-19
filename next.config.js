b57e76facb813c034def4b34d2b3e604d5d1a7a7
TrasherTravis <trashertravis@gmail.com>
2021-08-19 08:21:15 +0530

    deleted

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withImages = require('next-images')
-// const withSvgr = require('next-plugin-svgr');
-
-module.exports = withImages({
-  fileExtensions: ["jpg", "png", "gif", "webp", "svg"],
-  webpack(config, options) {
-    return config
-  }
-})
